2ebc3693fc3750a2bfa3402cb159f515a39f52aa
Lance LI <heuristack@gmail.com>
2017-11-21 12:15:51 +0800

    algorithms: backtracking algorithm to generate all permutations [combinatorics]

---
+++ sorting/permutation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void generate_all_permutations(vector<int> const & elements)
+{
+    static vector<int> permutation;
+    if (elements.size() == 0) {
+        for (auto e : permutation) cout << e;
+        cout << endl;
+    }
+    for (int i = 0; i < elements.size(); i++) {
+        permutation.push_back(elements[i]);
+        auto candicates = elements;
+        candicates.erase(candicates.begin() + i);
+        generate_all_permutations(candicates);
+        permutation.pop_back();
+    }
+}
+
+int main()
+{
+    vector<int> elements = {1, 2, 3};
+    generate_all_permutations(elements);
+
+    string sequence = "123";
+    do {
+        cout << sequence << endl;
+    }
+    while (next_permutation(sequence.begin(), sequence.end()));
+}
+
